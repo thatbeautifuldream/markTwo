2f4a2db6691e1916af02ca0fc10fb474d3d4ea5f
Milind Mishra⚡️ <milind.mishra4@gmail.com>
2022-08-31 21:33:21 +0530

    📦 NEW: added questions and logic

---
+++ bin/index.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+// Making Fandom Quiz CLI in Node.js
+
+// Importing the modules
+import chalk from "chalk";
+import clear from "clear";
+import figlet from "figlet";
+import { exit } from "process";
+import readline from "readline";
+import questions from "./questions.js";
+
+const log = console.log; // alias for console.log
+
+// readline module is used to read the input from the user
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// Clear the console
+clear();
+
+// Display the title
+log(
+  chalk.yellow(figlet.textSync("Fandom Quiz CLI", { horizontalLayout: "full" }))
+);
+
+// Display the description
+log(chalk.blue("This is a Fandom Quiz CLI made in Node.js"));
+
+// Display the author
+log(chalk.green("Author: Milind"));
+
+// Initializing the score to 0
+let score = 0;
+
+// Creating the ask function
+function ask(i) {
+  rl.question(questions[i].question, (userInput) => {
+    if (userInput.toLowerCase() === questions[i].answer.toLowerCase()) {
+      log(chalk.green("Right!"));
+      score++;
+      log(chalk.blue("Yay score++, current score : ", score));
+    } else {
+      log(chalk.red("Wrong!"));
+      log(chalk.red("The correct answer is: ", questions[i].answer));
+      log(chalk.red("Your score remains: ", score));
+    }
+
+    if (i < questions.length - 1) {
+      ask(i + 1);
+    } else {
+      log(chalk.yellow("Your final score is: " + score));
+      rl.close();
+    }
+  });
+}
+
+// Calling the ask function
+ask(0);
---
+++ bin/questions.js
@@ -0,0 +1,36 @@
+// programming quiz questions array
+const questions = [
+  {
+    question:
+      "What is the name of the programming language that is used to create this quiz?",
+    answer: "javascript",
+  },
+  {
+    question: "What is the runtime environment used to run this quiz?",
+    answer: "node",
+  },
+  {
+    question:
+      "What is the name of the package manager used to install this quiz?",
+    answer: "npm",
+  },
+  {
+    question:
+      "What are the other package manager available in the JavaScript ecosystem?",
+    answer: "npm, yarn and pnpm",
+  },
+  {
+    question: "What is the diffrence between let and var?",
+    answer: "let is block scoped and var is function scoped",
+  },
+  {
+    question: "What is the diffrence between const and let?",
+    answer: "const is block scoped and let is function scoped",
+  },
+  {
+    question: "What is the name of the author of this quiz?",
+    answer: "Milind",
+  },
+];
+
+export default questions;
